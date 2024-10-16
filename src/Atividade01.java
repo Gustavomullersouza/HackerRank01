551d3914b17ea2161362b5f57fe14060270c1fdc
gusta <gustavomullerdesouza@gmail.com>
2024-10-15 22:41:28 -0300

    HackerRank

---
+++ src/Atividade01.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Atividade01 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite o primeiro numero: ");
+        int a = scan.nextInt();
+        System.out.println("Digite o segundo numero: ");
+        int b = scan.nextInt();
+        System.out.println("Digite o terceiro numero: ");
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
